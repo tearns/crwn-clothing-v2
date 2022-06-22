a0e84a9f3e95b95c8a0a3103c599eb82bd6f09fc
tearns <thanyakarn.sue@gmail.com>
2022-06-23 00:42:04 +0700

    add our sign in and component as well as firebase utils anf brought in firebase libraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -44,4 +44,25 @@ export const createUserDocumentFromAuth = async(userAuth) => {
     const userSnapshot = await getDoc(userDocRef);
     console.log(userSnapshot);
     console.log(userSnapshot.exists());
-};
+
+    //if user data exits
+    if(!userSnapshot.exists()) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        }
+            catch(error){
+                console.log('error creating user',error.message);
+            }
+        
+        }
+        return userDocRef;
+    };
+
+    
